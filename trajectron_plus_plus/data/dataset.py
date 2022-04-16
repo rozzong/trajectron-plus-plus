4072be1e86231204e75e0e3a1251c3000ca21a62
Gabriel Rozzonelli <62215635+rozzong@users.noreply.github.com>
2022-04-16 16:47:20 +0200

    Remove uses of hyper parameters in the node type dataset
    * Updated NodeTypeIterableDataset in `trajectron_plus_plus/data/dataset.py`

--- trajectron_plus_plus/data/dataset.py
+++ trajectron_plus_plus/data/dataset.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterable, List, Mapping, Tuple
+from typing import Any, Dict, Iterable, List, Mapping, Optional, Tuple
 
 import numpy as np
 from torch.utils.data import IterableDataset
@@ -18,6 +18,16 @@ class NodeTypeIterableDataset(IterableDataset):
     edge_types : List[Tuple["NodeType", "NodeType"]]
         The list of all edge types to take into account when processing the
         node's neighbours.
+    include_robot : bool
+        True to include the ego-agent, False to not include it.
+    min_len_history : int
+        The minimum number of timesteps to use in the past.
+    max_len_history : int
+        The maximum number of timesteps to use in the past.
+    min_len_future : int
+        The minimum number of timesteps to use in the future.
+    max_len_future : int
+        The maximum number of timesteps to use in the future.
     standardization_params : Dict[str, Dict[str, Tuple[np.ndarray, np.ndarray]]]
         Standardization parameters for state and prediction state structures,
         for all nodes.
@@ -26,8 +36,20 @@ class NodeTypeIterableDataset(IterableDataset):
 
     attention_radius : Dict[Tuple[NodeType, NodeType], float]
         The dictionary holding all attention radii for all edge types.
+    state : Dict[str, Dict[str, List[str]]]
+        The dictionary defining all nodes states.
+    pred_state : Dict[str, Dict[str, List[str]]]
+        The dictionary defining all nodes states to be predicted.
     hyperparams : Mapping[str, Any]
         Model hyperparameters.
+    scene_multiplier : int, default: 1
+        The number of times to iterate over each scene.
+    node_multiplier : int, default: 1
+        The number of times to iterate over each node in a scene.
+    edge_addition_filter : Optional[List[float]], default: None
+
+    edge_removal_filter : Optional[List[float]], default: None
+
     """
 
     def __init__(
@@ -35,22 +57,42 @@ class NodeTypeIterableDataset(IterableDataset):
             scenes: Iterable["Scene"],
             node_type: "NodeType",
             edge_types: List[Tuple["NodeType", "NodeType"]],
+            include_robot: bool,
+            min_len_history: int,
+            max_len_history: int,
+            min_len_future: int,
+            max_len_future: int,
             standardization_params: Dict[str, Dict[str, Tuple[np.ndarray, np.ndarray]]],
             attention_radius: Dict[Tuple["NodeType", "NodeType"], float],
+            state: Dict[str, Dict[str, List[str]]],
+            pred_state: Dict[str, Dict[str, List[str]]],
             hyperparams: Mapping[str, Any],
             scene_multiplier: int = 1,
             node_multiplier: int = 1,
+            edge_addition_filter: Optional[List[float]] = None,
+            edge_removal_filter: Optional[List[float]] = None,
     ):
         super().__init__()
+
+        # TODO: Get rid of hyperparams completely
 
         self.scenes = scenes
         self.node_type = node_type
         self.edge_types = edge_types
+        self.include_robot = include_robot
+        self.min_len_history = min_len_history
+        self.max_len_history = max_len_history
+        self.min_len_future = min_len_future
+        self.max_len_future = max_len_future
         self.standardization_params = standardization_params
         self.attention_radius = attention_radius
+        self.state = state
+        self.pred_state = pred_state
         self.hyperparams = hyperparams
         self.scene_multiplier = scene_multiplier
         self.node_multiplier = node_multiplier
+        self.edge_addition_filter = edge_addition_filter
+        self.edge_removal_filter = edge_removal_filter
 
         # Check edge types
         self.edge_types = [
@@ -66,16 +108,16 @@ class NodeTypeIterableDataset(IterableDataset):
             present_nodes_dict = scene.present_nodes(
                 np.arange(scene.timesteps),
                 type=self.node_type,
-                min_history_timesteps=self.hyperparams["minimum_history_length"],
-                min_future_timesteps=self.hyperparams["prediction_horizon"],
-                return_robot=self.hyperparams["include_robot"]
+                min_history_timesteps=self.min_len_history,
+                min_future_timesteps=self.min_len_future,
+                return_robot=self.include_robot
             )
             for timestep, nodes in present_nodes_dict.items():
                 scene_graph = scene.get_scene_graph(
                     timestep,
                     self.attention_radius,
-                    self.hyperparams["edge_addition_filter"],
-                    self.hyperparams["edge_removal_filter"]
+                    self.edge_addition_filter,
+                    self.edge_removal_filter
                 )
                 for node in nodes:
                     yield from [
@@ -83,13 +125,13 @@ class NodeTypeIterableDataset(IterableDataset):
                             scene,
                             timestep,
                             node,
-                            self.hyperparams["state"],
-                            self.hyperparams["pred_state"],
+                            self.state,
+                            self.pred_state,
                             self.edge_types,
                             self.standardization_params,
                             self.attention_radius,
-                            self.hyperparams["maximum_history_length"],
-                            self.hyperparams["prediction_horizon"],
+                            self.max_len_history,
+                            self.max_len_future,
                             self.hyperparams,
                             scene_graph
                         )
